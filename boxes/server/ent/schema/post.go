bdcb738b01eca1e3a493af50115c03874c92da4c
Ray <raydatray@gmail.com>
2025-08-17 22:40:23 -0400

    add fields and edges to post

--- boxes/server/ent/schema/post.go
+++ boxes/server/ent/schema/post.go
@@ -1,18 +1,32 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
-// Post holds the schema definition for the Post entity.
 type Post struct {
 	ent.Schema
 }
 
-// Fields of the Post.
 func (Post) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("title").NotEmpty(),
+		field.Text("content").NotEmpty(),
+	}
 }
 
-// Edges of the Post.
 func (Post) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("author", User.Type).
+			Ref("posts").
+			Unique().
+			Required(),
+		edge.From("board", DiscussionBoard.Type).
+			Ref("posts").
+			Unique().
+			Required(),
+		edge.To("replies", Reply.Type),
+	}
 }
