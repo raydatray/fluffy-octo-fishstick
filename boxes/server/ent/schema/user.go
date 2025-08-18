73e42e4ceab7be82d7662911d5c9c17858a0c3eb
Ray <raydatray@gmail.com>
2025-08-17 22:00:37 -0400

    add basic fields to user

--- boxes/server/ent/schema/user.go
+++ boxes/server/ent/schema/user.go
@@ -1,18 +1,21 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
-// User holds the schema definition for the User entity.
 type User struct {
 	ent.Schema
 }
 
-// Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("email").Unique(),
+		field.String("password"),
+	}
 }
 
-// Edges of the User.
 func (User) Edges() []ent.Edge {
 	return nil
 }
